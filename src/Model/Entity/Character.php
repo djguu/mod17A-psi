8f7dbff2fa8bfac5cfd37be53f48d54538d4d303
Gonçalo Correia <goncalo.correia@rupeal.com>
2017-01-28 15:45:10 +0000

    removed unnecessary lines

--- src/Model/Entity/Character.php
+++ src/Model/Entity/Character.php
@@ -3,31 +3,8 @@ namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
-/**
- * Character Entity
- *
- * @property int $id
- * @property string $name
- * @property string $gender
- * @property int $user_id
- *
- * @property \App\Model\Entity\User[] $users
- * @property \App\Model\Entity\Card[] $cards
- */
+
 class Character extends Entity
 {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        '*' => true,
-        'id' => false
-    ];
 }
